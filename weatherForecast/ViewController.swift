523334f25c0e1c5d3915e1f6608c8e3ec8eb5d30
松村敬太 <MatsumuraKeita@matsumuratakashifutoshi-no-imac.local>
2018-04-22 23:07:27 +0900

    Initial Commit

---
+++ weatherForecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherForecast
+//
+//  Created by 松村敬太 on 2018/04/22.
+//  Copyright © 2018年 KeitaMatsumura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
