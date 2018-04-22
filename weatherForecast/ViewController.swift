62fb9975f6408f27e43d82155565db47715ed64a
松村敬太 <MatsumuraKeita@matsumuratakashifutoshi-no-imac.local>
2018-04-23 01:50:40 +0900

    動作確認

--- weatherForecast/ViewController.swift
+++ weatherForecast/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
     
     
@@ -17,9 +17,65 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cityLabel: UILabel!
     
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        cityLabel.text = cityTextField.text
+        cityTextField.resignFirstResponder()
+        return true
+    }
+    
     @IBAction func findWeather(_ sender: Any) {
         super.viewDidLoad()
+        
+        var success = false
+        
+//        let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" + self.cityTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+
+        let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" + self.cityTextField.text! + "/forecasts/latest")
+
+        if let url = attemptedUrl {
+            
+            let task = URLSession.shared.dataTask(with: url as URL) { (data, response, error) -> Void in
+                
+                if let urlContent = data {
+                    //データがnilではない
+                    print(url)
+                    let webContent = NSString(data: urlContent, encoding: String.Encoding.utf8.rawValue)
+                    
+                    let websiteArray = webContent?.components(separatedBy: "3 days)</span><p class=\"b-forecast__table-description-content\"><span class=\"phrase\">")
+                    
+                    if websiteArray!.count > 1 {
+                        print("websiteArray>1")
+//                        let weatherArray = websiteArray![1].componentsSeparatedByString("</span>")
+                        let weatherArray = websiteArray![1].components(separatedBy: "</span>")
+                        
+                        if weatherArray.count > 1 {
+                            print("weatherArray>1")
+//                            let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "°")
+                            let weatherSummary = weatherArray[0].replacingOccurrences(of: "&deg;", with:  "°")
+                            
+//                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//
+//                                success = true
+//
+//                                self.resultLabel.text = weatherSummary
+//                            }
+                            DispatchQueue.main.async {() -> Void in
+                                
+                            success = true
+                                
+                            self.resultLabel.text = weatherSummary
+                            }
+                        }
+                    }
+                }
+            }
+            
+            task.resume()
+            
+            if success == false{
+                self.resultLabel.text = "Sorry, something went wrong."
+            }
+        }
         
     }
     override func viewDidLoad() {
